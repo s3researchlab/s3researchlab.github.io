78ec5f317fd1f531117c2c5079a6a265bcb1f4a8
Thiago Ferreira <thiagodnf@gmail.com>
2022-08-15 14:06:50 -0400

    several improvements

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -28,13 +28,16 @@ function NavBar() {
 
     return (
         <Navbar expand="md" fixed="top" id="sideNav" bg="light">
-            <Container >
-                <Navbar.Brand href="/">S3 Research Lab</Navbar.Brand>
+            <Container className="py-2">
+
+                <img className="img-fluid d-block " src="images/logo-long-name-transparent.png" width={500} />
+                {/* <Navbar.Brand href="/">S3 Research Lab</Navbar.Brand> */}
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
                     <Nav>
                         <NavLink href="/" exact>Home</NavLink>
                         <NavLink href="/team">Team</NavLink>
+                        <NavLink href="/projects">Projects</NavLink>
                         <NavLink href="/publications">Publications</NavLink>
                         <NavLink href="/contact-us">Contact Us</NavLink>
                     </Nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
+import { Col, Container, Row } from "react-bootstrap";
 
 function IndexPage() {
 
     return (
         <>
+            <Container>
 
-           <p>Home</p>
+                <Row className="mt-5">
+                    <Col sm="12">
+                        <p class="text-primary text-uppercase mb-5">Welcome to S3 Research Lab</p>
 
-            <p>Goal: Ensure the use of smart technologies in all aspects of the design, implementation, evaluation, and verification of software systems</p>
+                        <h3 class="mb-5">Our goal is to research the use of smart technologies in all aspects of the design, implementation, evaluation, and verification of software and systems</h3>
+
+                        <button className="btn btn-grad-primary">Explore</button>
+                    </Col>
+
+                </Row>
+
+            </Container>
 
         </>
     );
