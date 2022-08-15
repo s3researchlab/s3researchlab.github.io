b3a3c570e5db68321eb2acefa0e72398424c7383
Thiago Ferreira <thiagodnf@gmail.com>
2022-08-14 20:11:26 -0400

    some refactorings

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -16,13 +16,11 @@ const injectGA = () => {
 
 function Footer() {
 
-    const config = getConfig().publicRuntimeConfig;
-
     return (
         <div>
             <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossOrigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
-            <script src={`${config.basePath}/js/script.js`}/>
+            <script src="js/script.js"/>
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-HNGSCV50W9"></script>
             <script>{injectGA()}</script>
         </div>
--- src/layout/Head.js
+++ src/layout/Head.js
@@ -1,19 +1,16 @@
 import Head from "next/head";
-import getConfig from "next/config";
 
 import Package from "../../package.json";
 
 function Component({page}) {
 
-    const config = getConfig().publicRuntimeConfig;
-
-    const title = `${page} - ${Package.author}`;
+    const title = `${Package.author}`;
 
     return (
         <Head>
             <meta charSet="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <title>{title}</title>
+            <title>S3 Research Lab</title>
 
             <meta name="author" content={Package.author} />
             <meta name="description" content={Package.description} />
@@ -25,16 +22,16 @@ function Component({page}) {
             <meta name="apple-mobile-web-app-status-bar-style" content="black" />
             <meta name="apple-mobile-web-app-title" content={Package.author}/>
 
-            <link rel="apple-touch-icon" sizes="180x180" href={`${config.basePath}/images/profile.jpg`}/>
-            <link rel="icon" type="image/png" href={`${config.basePath}/images/favicons/favicon-32x32.jpg`}/>
-            <link rel="icon" type="image/png" sizes="32x32" href={`${config.basePath}/images/favicons/favicon-32x32.jpg`}/>
-            <link rel="icon" type="image/png" sizes="16x16" href={`${config.basePath}/images/favicons/favicon-16x16.jpg`}/>
-            <link rel="manifest" href={`${config.basePath}/images/favicons/site.webmanifest`}></link>
+            <link rel="apple-touch-icon" sizes="180x180" href="images/profile.jpg"/>
+            <link rel="icon" type="image/png" href="images/favicons/favicon-32x32.jpg"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.jpg"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.jpg"/>
+            <link rel="manifest" href="images/favicons/site.webmanifest"></link>
 
             <meta property="og:title" content={Package.author} />
             <meta property="og:url" content={Package.homepage} />
             <meta property="og:description" content={Package.description} />
-            <meta property="og:image" content={`${config.basePath}/images/profile.jpg`} />
+            <meta property="og:image" content="images/profile.jpg" />
             <meta property="og:locale" content="en_US" />
             <meta property="og:locale:alternate" content="pt_BR" />
 
