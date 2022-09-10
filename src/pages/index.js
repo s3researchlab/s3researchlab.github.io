a45cd1ec1072ba1eed6246f15272aed93a6dac48
Thiago Ferreira <thiagodnf@gmail.com>
2022-09-09 21:15:12 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ function IndexPage() {
                     <Col sm="12">
                         <p class="text-primary text-uppercase mb-5">Welcome to S3 Research Lab</p>
 
-                        <h3 class="mb-5">Our goal is to research the use of smart technologies in all aspects of the design, implementation, evaluation, and verification of software and systems</h3>
+                        <h3 class="mb-5">Our goal is to research the use of smart technologies in all aspects of the design, implementation, evaluation, and verification of software systems</h3>
 
                         <button className="btn btn-grad-primary">Explore</button>
                     </Col>
