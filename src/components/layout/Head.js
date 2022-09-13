c21970e91625c56d53b880990dd3bc61f6f508ab
Thiago Ferreira <thiagodnf@gmail.com>
2022-09-13 14:21:45 -0400

    some improvements

--- src/components/layout/Head.js
+++ src/components/layout/Head.js
@@ -17,17 +17,11 @@ function Component({ title }) {
             <meta name="description" content={Package.description} />
             <meta name="keywords" content={Package.keywords} />
             <meta name="robots" content="index, follow" />
-            <meta name="theme-color" content="#021329" />
+            <meta name="theme-color" content="#120B1C" />
             <meta name="mobile-web-app-capable" content="yes" />
             <meta name="apple-mobile-web-app-capable" content="yes" />
             <meta name="apple-mobile-web-app-status-bar-style" content="black" />
             <meta name="apple-mobile-web-app-title" content={Package.author} />
-
-            {/* <link rel="apple-touch-icon" sizes="180x180" href="images/profile.jpg"/>
-            <link rel="icon" type="image/png" href="images/favicons/favicon-32x32.jpg"/>
-            <link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.jpg"/>
-            <link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.jpg"/>
-            <link rel="manifest" href="images/favicons/site.webmanifest"></link> */}
 
             <meta property="og:title" content={Package.author} />
             <meta property="og:url" content={Package.homepage} />
