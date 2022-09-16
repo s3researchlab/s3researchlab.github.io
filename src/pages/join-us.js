45e8d36152302c947c9255063c35db0eafb6e487
Thiago Ferreira <thiagodnf@gmail.com>
2022-09-15 20:10:14 -0400

    add urop

--- src/pages/join-us.js
+++ src/pages/join-us.js
@@ -5,11 +5,15 @@ function JoinUsPage() {
     return (
         <Layout title="Join Us">
 
-            <h4>General Audience</h4>
-
             <p>We're always looking for talented, motivated people to join us. If you're interested in the things we do and you'd like to join us, or visit our lab, please contact Thiago Ferreira.</p>
 
+            <h5>UM-Flint Undergraduate Student</h5>
+
             <p>If you are a UM-Flint undergraduate student looking for research experience, please consider joining the lab through the UROP program. Click on the figure below.</p>
+
+            <a href="https://umflint.infoready4.com/#urop">
+                <img src="https://user-images.githubusercontent.com/114015/190529558-398650e0-5356-48e2-866b-0b1aa63e126d.jpg" />
+            </a>
         </Layout>
     );
 }
