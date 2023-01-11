b04a550dfac3f287ebea6b366ef4abd8c533bfd7
Thiago Ferreira <thiagodnf@gmail.com>
2023-01-10 22:09:02 -0500

    new images

--- src/components/layout/Head.js
+++ src/components/layout/Head.js
@@ -23,15 +23,15 @@ function Component({ title }) {
             <meta name="apple-mobile-web-app-status-bar-style" content="black" />
             <meta name="apple-mobile-web-app-title" content={Package.author} />
 
-            <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png"/>
-            <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png"/>
-            <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.jpg"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.jpg"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.jpg"/>
             <link rel="manifest" href="/images/favicons/site.webmanifest"/>
 
             <meta property="og:title" content={Package.author} />
             <meta property="og:url" content={Package.homepage} />
             <meta property="og:description" content={Package.description} />
-            <meta property="og:image" content="/images/favicons/favicon-32x32.png" />
+            <meta property="og:image" content="/images/favicons/favicon-32x32.jpg" />
             <meta property="og:locale" content="en_US" />
             <meta property="og:locale:alternate" content="pt_BR" />
 
