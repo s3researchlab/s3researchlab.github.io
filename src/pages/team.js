29fe0f5fd3d1bc63fcf719c1c1322f695e8728ef
Thiago Ferreira <thiagodnf@gmail.com>
2023-10-13 22:45:24 -0400

    Update team.js

--- src/pages/team.js
+++ src/pages/team.js
@@ -7,13 +7,13 @@ function Member({ id, name, position, img, url, when }) {
 
     return <>
         <div className="member" key={id}>
-            <div class="d-flex">
-                <div class="flex-shrink-0">
+            <div className="d-flex">
+                <div className="flex-shrink-0">
                     <a href={url} target="_blank">
                         <img className="rounded" src={`images/profile-photo/${img}`} width={65} />
                     </a>
                 </div>
-                <div class="flex-grow-1 ms-2">
+                <div className="flex-grow-1 ms-2">
                     <p className="text-dark fw-bold mb-0">{name}</p>
                     <p className="mb-0">{position}</p>
                     <p className="small text-start text-secondary">{when}</p>
