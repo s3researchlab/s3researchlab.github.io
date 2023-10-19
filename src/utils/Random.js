1e078ca13d0d8b7da5bc62e625f4ad8313d9a84e
Thiago Ferreira <thiagodnf@gmail.com>
2023-10-19 09:11:46 -0400

    added some jsdoc

--- src/utils/Random.js
+++ src/utils/Random.js
@@ -1,9 +1,19 @@
 export default class Random {
 
+    /**
+     * @param {number} min the lower number
+     * @param {number} max the higher number (inclusive)
+     * @returns a random number between min and max (both included):
+     */
     static nextInt(min, max) {
+
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
+    /**
+     * @param {array} array the elements to be selected
+     * @returns a random element from the array
+     */
     static nextEl(array) {
 
         const index = Random.nextInt(0, array.length - 1);
